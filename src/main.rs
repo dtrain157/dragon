8ebfb45839042982e8a78b7f54580681eb3a7553
Derrick van Zyl <derrick.vanzyl@gmail.com>
2020-07-09 23:13:31 +0100

    adding vga_buffer.rs: allows us to write to the VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 #![no_std]
 #![no_main]
 
+pub mod vga_buffer;
+
+use core::fmt::Write;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xB8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset((i as isize * 2) + 1) = 0xB;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("this is the end!");
 
     loop {}
 }
